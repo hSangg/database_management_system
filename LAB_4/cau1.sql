673c6b7aea35e8a5940e75de3dc10d80748ac1c2
bangtran <89561730+bangtran-17@users.noreply.github.com>
2023-05-09 14:47:20 +0700

    Update cau1.sql

--- LAB_4/cau1.sql
+++ LAB_4/cau1.sql
@@ -14,25 +14,12 @@ BEGIN
       END IF;
     END;
 
-CREATE OR REPLACE TRIGGER TG_2
-AFTER UPDATE OR INSERT ON KHACHHANG
-FOR EACH ROW 
-DECLARE 
-    NGHD1 HOADON.NGHD%TYPE;
-    NGDK1 KHACHHANG.NGDK%TYPE;
-    MAKH1 HOADON.MAKH%TYPE;
-BEGIN
-    SELECT NGHD INTO NGHD1 FROM HOADON WHERE MAKH=:NEW.MAKH;
-    IF :NEW.NGDK>NGHD1 THEN
-      RAISE_APPLICATION_ERROR(-20001, 'Ngày mua hàng phải lớn hơn hoặc bằng ngày đăng ký thành viên.');
-      END IF;
-    END;
+
     --bang tam anh huong
 /*
 +-----------+------+-----+---------+
 |   Bảng    | thêm | xóa |   sửa   |
 +-----------+------+-----+---------+
-| KHACHHANG | +    |   - | +(NGHD) |
 | HOADON    | +    |   - | +(NGDK) |
 +-----------+------+-----+---------+
 */
